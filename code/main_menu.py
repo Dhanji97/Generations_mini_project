7c79bf6fb033c5e35e375d7290cc044d9b3ed5ef
Dhanji97 <m-a.dhanji@hotmail.com>
2021-07-21 09:48:59 +0100

    Fixed write to file bug in main_menu exit option

--- code/main_menu.py
+++ code/main_menu.py
@@ -21,14 +21,14 @@ def main_menu():
     
     if main_menu_input == 0:
         try:
-            with open('mini_project_app/products.txt', 'w') as products_file:
+            with open('products.txt', 'w') as products_file:
                 for product in information.products_list:
                     products_file.write(product + '\n')
         except Exception as e:
             print("File 'products.txt' could not be opened" + str(e))
         
         try:
-            with open('mini_project_app/couriers.txt', 'w') as couriers_file:
+            with open('couriers.txt', 'w') as couriers_file:
                 for courier in information.couriers_list:
                     couriers_file.write(courier + '\n')
         except Exception as e:
