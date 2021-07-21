e9b8ce9a1cbb5ef08458e3511eaaf87269a544b0
Dhanji97 <m-a.dhanji@hotmail.com>
2021-07-21 12:05:07 +0100

    Implemented the update order feature in the Orders_menu

--- code/Orders_menu.py
+++ code/Orders_menu.py
@@ -33,8 +33,7 @@ def Orders_menu():
         update_Order_status()
     
     elif Orders_menu_input == 4:
-        print('not yet implimented')
-        # update_Order()
+        update_Order()
     
     elif Orders_menu_input == 5:
         delete_Order()
@@ -69,9 +68,9 @@ def new_Order():
     order_dict =    {
                     'Customer name': customer_name,
                     'Customer address': customer_address,
-                    'customer_phone_num': customer_phone_num,
-                    'courier': courier_index,
-                    'status': 'preparing'
+                    'Customer phone number': customer_phone_num,
+                    'Courier': courier_index,
+                    'Status': 'preparing'
                     }
     
     information.Orders_list.append(order_dict)
@@ -103,7 +102,7 @@ def update_Order_status():
     # C added to valid inputs to allow user to cancel selection and return to previous menu
     valid_status_inputs.append('C')
     
-    status_index = input('Please select a status to updatethe order\'s status too or C to cancel: ')
+    status_index = input('Please select a status to update the order\'s status too or C to cancel: ')
     if status_index not in valid_status_inputs:
         print('Sorry that option was not recognized')
         return update_Order_status()
@@ -115,27 +114,56 @@ def update_Order_status():
     print(f'Order\'s status successfully updated to {updated_status}')
     return Orders_menu()
 
-# def update_Order():
-#     '''updates the name of an existing Order in the Order list'''
-#     for i, Order in enumerate(information.Orders_list):
-#         print(i, Order)
+def update_Order():
+    '''updates the entires of an existing Order in the Order list'''
+    for i, Order in enumerate(information.Orders_list):
+        print(i, Order)
     
-#     valid_update_inputs = [str(i) for i in range(len(information.Orders_list))]
-#     # C added to valid inputs to allow user to cancel selection and return to previous menu
-#     valid_update_inputs.append('C')
-#     update_index = input('select Order number to update or C to cancel: ')
-#     if update_index not in valid_update_inputs:
-#         print('Sorry that option was not recognized\n')
-#         return update_Order()
-#     elif update_index == 'C':
-#         return Orders_menu()
+    valid_update_inputs = [str(i) for i in range(len(information.Orders_list))]
+    # C added to valid inputs to allow user to cancel selection and return to previous menu
+    valid_update_inputs.append('C')
+    update_index = input('select Order number to update or C to cancel: ')
+    if update_index not in valid_update_inputs:
+        print('Sorry that option was not recognized\n')
+        return update_Order()
+    elif update_index == 'C':
+        return Orders_menu()
     
-#     update_index = int(update_index)
+    update_index = int(update_index)
+    Order_dict_to_update = information.Orders_list[update_index]
+    print(Order_dict_to_update)
     
-#     updated_Order_name = input('Please enter new Order name: ')
-#     information.Orders_list[update_index] = updated_Order_name
-#     print(f'Order updated to "{updated_Order_name}".')
-#     return Orders_menu()
+    update_customer_name = input('Update customer name, press enter to skip:\n')
+    if update_customer_name == '':
+        pass
+    else:
+        Order_dict_to_update['Customer name'] = update_customer_name
+    
+    update_customer_house_number = input('Update customer house number, press enter to skip:\n')
+    update_customer_postcode = input('Update customer postcode, press enter to skip:\n')
+    original_address = Order_dict_to_update['Customer address'].split(', ')
+    if update_customer_house_number == '':
+        update_customer_house_number = original_address[0]
+    if update_customer_postcode == '':
+        update_customer_postcode = original_address[1]
+    updated_address = update_customer_house_number + ', ' + update_customer_postcode
+    Order_dict_to_update['Customer address'] = updated_address
+    
+    update_customer_phone_number = input('Update customer phone number, press enter to skip:\n')
+    if update_customer_phone_number == '':
+        pass
+    else:
+        Order_dict_to_update['Customer phone number'] = update_customer_phone_number
+    
+    current_courier = Order_dict_to_update['Courier']
+    updated_courier = update_order_courier_option(current_courier)
+    Order_dict_to_update['Courier'] = updated_courier
+    
+    current_status = Order_dict_to_update['Status']
+    updated_status = update_order_status_option(current_status)
+    Order_dict_to_update['Status'] = updated_status
+    print(f'Order updated too:\n {Order_dict_to_update}\n')
+    return Orders_menu()
 
 def delete_Order():
     '''deletes an Order in the Orders list'''
@@ -157,3 +185,40 @@ def delete_Order():
     information.Orders_list.pop(delete_index)
     print('The order was removed from orders list.')
     return Orders_menu()
+
+def update_order_courier_option(current_courier_option):
+    for i, courier in enumerate(information.couriers_list):
+        print(i, courier)
+    
+    valid_courier_inputs = [str(i) for i in range(len(information.couriers_list))]
+    # 'blank' added to valid inputs to allow user to skip selection and continue updating order
+    valid_courier_inputs.append('')
+    
+    courier_index = input('Please select a courier with its indicated number or press enter to skip: ')
+    
+    if courier_index not in valid_courier_inputs:
+        print('Sorry that option was not recognized')
+        return update_order_courier_option(current_courier_option)
+    elif courier_index == '':
+        return current_courier_option
+    else:
+        return int(courier_index)
+
+def update_order_status_option(current_status_option):
+    for i, status in enumerate(information.Order_status_list):
+        print(i, status)
+    
+    valid_status_inputs = [str(i) for i in range(len(information.Order_status_list))]
+    # 'blank' added to valid inputs to allow user to skip selection and continue updating order
+    valid_status_inputs.append('')
+    
+    status_index = input('Please select a status with its indicated number or press enter to skip: ')
+    
+    if status_index not in valid_status_inputs:
+        print('Sorry that option was not recognized')
+        return update_order_status_option(current_status_option)
+    elif status_index == '':
+        return current_status_option
+    else:
+        status_index = int(status_index)
+        return information.Order_status_list[status_index]
--- code/information.py
+++ code/information.py
@@ -10,5 +10,11 @@ try:
 except Exception as e:
     print("File 'couriers.txt' could not be opened\n" + str(e))
 
-Orders_list = []
+Orders_list = [{
+                    'Customer name': 'Dhanji',
+                    'Customer address': '1, AB1 2CD',
+                    'Customer phone number': '07123456789',
+                    'Courier': 1,
+                    'Status': 'preparing'
+                    }]
 Order_status_list = ['Preparing', 'Out for divlivery']
