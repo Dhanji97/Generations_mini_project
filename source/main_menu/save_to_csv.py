058cef76e260c82e7b09b2ef4cd75aa4a8e37e8e
Dhanji97 <m-a.dhanji@hotmail.com>
2021-07-23 12:03:14 +0100

    Refactored save_to_csv to allow for data injection for file location

--- source/main_menu/save_to_csv.py
+++ source/main_menu/save_to_csv.py
@@ -1,26 +1,9 @@
 import csv
 
-random_list_dict = [{
-                    'Customer name': 'Dhanji',
-                    'Customer address': '1, AB1 2CD',
-                    'Customer phone number': '07147258369',
-                    'Courier': 1,
-                    'Status': 'preparing'
-                    },
-                {
-                    'Customer name': 'Abbas',
-                    'Customer address': '2, EF3 4GH',
-                    'Customer phone number': '07963852741',
-                    'Courier': 1,
-                    'Status': 'Out for delivery'
-                    }]
-
-random_list_dict_2 = [{'name': 'Dave', 'number': '07123456789'}, {'name': 'Bob', 'number': '07987654321'}]
-
-def save_products(list_of_dict):
+def save_products(list_of_dict, file_name):
     field_names = list_of_dict[0].keys()
     try:
-        with open('source/saved_information/products.csv','w', newline="") as file:
+        with open(file_name,'w', newline="") as file:
             writer = csv.DictWriter(file, field_names)
             writer.writeheader()
             writer.writerows(list_of_dict)
@@ -28,10 +11,10 @@ def save_products(list_of_dict):
     except Exception as e:
         print(e)
 
-def save_couriers(list_of_dict):
-    field_names = ['name', 'number']
+def save_couriers(list_of_dict, file_name):
+    field_names = list_of_dict[0].keys()
     try:
-        with open('source/saved_information/couriers.csv','w', newline="") as file:
+        with open(file_name,'w', newline="") as file:
             writer = csv.DictWriter(file, field_names)
             writer.writeheader()
             writer.writerows(list_of_dict)
@@ -39,15 +22,13 @@ def save_couriers(list_of_dict):
     except Exception as e:
         print(e)
 
-def save_orders(list_of_dict):
+def save_orders(list_of_dict, file_name):
     field_names = list_of_dict[0].keys()
     try:
-        with open('source/saved_information/orders.csv','w', newline="") as file:
+        with open(file_name,'w', newline="") as file:
             writer = csv.DictWriter(file, field_names)
             writer.writeheader()
             writer.writerows(list_of_dict)
             print('Orders saved successfully')
     except Exception as e:
         print(e)
-save_couriers(random_list_dict_2)
-# save_orders(random_list_dict)
