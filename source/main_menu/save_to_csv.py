23261538ddc510abd7ec2d1e818ffd3938877652
Dhanji97 <m-a.dhanji@hotmail.com>
2021-07-28 14:04:55 +0100

    Removed repetitive code in place of single function

--- source/main_menu/save_to_csv.py
+++ source/main_menu/save_to_csv.py
@@ -1,34 +1,12 @@
 import csv
 
-def save_products(list_of_dict, file_name):
+def save_csv(list_of_dict, file_name, print_message):
     field_names = list_of_dict[0].keys()
     try:
         with open(file_name,'w', newline="") as file:
             writer = csv.DictWriter(file, field_names)
             writer.writeheader()
             writer.writerows(list_of_dict)
-            print('Prodcts saved successfully')
-    except Exception as e:
-        print(e)
-
-def save_couriers(list_of_dict, file_name):
-    field_names = list_of_dict[0].keys()
-    try:
-        with open(file_name,'w', newline="") as file:
-            writer = csv.DictWriter(file, field_names)
-            writer.writeheader()
-            writer.writerows(list_of_dict)
-            print('Couriers saved successfully')
-    except Exception as e:
-        print(e)
-
-def save_orders(list_of_dict, file_name):
-    field_names = list_of_dict[0].keys()
-    try:
-        with open(file_name,'w', newline="") as file:
-            writer = csv.DictWriter(file, field_names)
-            writer.writeheader()
-            writer.writerows(list_of_dict)
-            print('Orders saved successfully')
+            print(print_message)
     except Exception as e:
         print(e)
