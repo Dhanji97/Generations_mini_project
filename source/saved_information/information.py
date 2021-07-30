7e9d230d9b554f06ac38aabd6c990ea59c8904ac
Dhanji97 <m-a.dhanji@hotmail.com>
2021-07-30 11:27:11 +0100

    removed legacy txt file storage

--- source/saved_information/information.py
+++ source/saved_information/information.py
@@ -1,17 +1,5 @@
 import csv
 
-try:
-    with open('products.txt', 'r') as products_file:      
-        products_list = [product.strip() for product in products_file.readlines()]
-except Exception as e:
-    print("File 'products.txt' could not be opened\n" + str(e))
-
-try:
-    with open('couriers.txt', 'r') as couriers_file:
-        couriers_list = [courier.strip() for courier in couriers_file.readlines()]
-except Exception as e:
-    print("File 'couriers.txt' could not be opened\n" + str(e))
-
 Orders_list = [{
                     'Customer name': 'Dhanji',
                     'Customer address': '1, AB1 2CD',
