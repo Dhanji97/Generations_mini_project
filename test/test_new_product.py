0b39adaa517a6e32dfa2c981b04ae50cc2f3dc5e
Dhanji97 <m-a.dhanji@hotmail.com>
2021-08-03 14:15:53 +0100

    added further units tests for test_new_product

--- test/test_new_product.py
+++ test/test_new_product.py
@@ -1,4 +1,3 @@
-import builtins
 from source.products_menu.new_product import create_new_product
 from unittest.mock import patch, Mock
 
@@ -9,4 +8,28 @@ def test_new_product(mock_input):
     mock_list.return_value = []
     create_new_product(mock_list)
     mock_list.append.assert_called_with({'name': 'new', 'price': '£1.00'})
-    
+
+@patch("builtins.input", side_effect=["new", '1.00'])
+@patch("builtins.print")
+def test_new_product_print(mock_print, mock_input):
+    mock_input.return_value
+    mock_list = Mock()
+    mock_list.return_value = []
+    create_new_product(mock_list)
+    mock_print.assert_called_with("{'name': 'new', 'price': '£1.00'} added to list of couriers")
+
+@patch("builtins.input")
+def test_new_product_first_C(mock_input):
+    mock_input.return_value = 'C'
+    mock_list = Mock()
+    mock_list.return_value = []
+    expected = create_new_product(mock_list)
+    assert expected == None
+
+@patch("builtins.input", side_effect=["new", 'C'])
+def test_new_product_second_C(mock_input):
+    mock_input.return_value
+    mock_list = Mock()
+    mock_list.return_value = []
+    expected = create_new_product(mock_list)
+    assert expected == None
