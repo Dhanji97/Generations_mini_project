1ef9302d591e18e3147ff63a6b944365af9827d9
Dhanji97 <m-a.dhanji@hotmail.com>
2021-10-29 03:18:27 +0100

    added __name__ == 'main' for good practice

--- source/run.py
+++ source/run.py
@@ -4,5 +4,8 @@ from couriers_menu import couriers_menu
 from orders_menu import orders_menu
 
 #Required to run the app and avoid circular imports 
+def main():
+    main_menu.main_menu()
 
-main_menu.main_menu()
+if __name__ == "__main__":
+    main()
